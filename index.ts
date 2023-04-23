c991b80ddd316ebbf748a30489ca2b9be974c87e
Larry W <l422y@l422y.com>
2023-04-22 20:45:45 -0400

    Refactor code to export PluginConfigProvider along with plugins and PluginLoader

--- index.ts
+++ index.ts
@@ -1,3 +1,11 @@
-import BasePlugin  from "./plugins/BasePlugin"
-import MonitoringPlugin  from "./plugins/MonitoringPlugin"
-import loadPlugins  from "./plugins/PluginLoader"
+import BasePlugin from "./plugins/BasePlugin"
+import MonitoringPlugin from "./plugins/MonitoringPlugin"
+import PluginLoader from "./plugins/PluginLoader"
+import PluginConfigProvider from "./plugins/PluginConfigProvider"
+
+export {
+    PluginConfigProvider,
+    BasePlugin,
+    MonitoringPlugin,
+    PluginLoader,
+}
