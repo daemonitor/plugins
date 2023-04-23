7015657aec0f62b0d4bcebbb4b2714dd0203cb7f
Larry W <l422y@l422y.com>
2023-04-22 20:47:01 -0400

    Refactor PluginLoader to a class and add a static method to load plugins from a directory in @daemonitor/plugins

--- plugins/PluginLoader.ts
+++ plugins/PluginLoader.ts
@@ -1,30 +1,29 @@
 import * as fs from "fs/promises"
 import * as path from "path"
-import BasePlugin from "~/lib/plugins/BasePlugin"
-import MonitoringPlugin from "~/lib/plugins/MonitoringPlugin"
+import BasePlugin from "@daemonitor/plugins/BasePlugin"
+import MonitoringPlugin from "@daemonitor/plugins/MonitoringPlugin"
 
-async function loadPlugins(): Promise<Array<BasePlugin | MonitoringPlugin>> {
-    const pluginsPath = path.join(__dirname, "../plugins")
-    const pluginFiles = ( await fs.readdir(pluginsPath) ).filter(file => /\.ts$/.test(file))
-    const plugins: Array<BasePlugin | MonitoringPlugin> = []
+export default class PluginLoader {
+    static async loadPlugins(): Promise<Array<BasePlugin | MonitoringPlugin>> {
+        const pluginsPath = path.join(__dirname, "../plugins")
+        const pluginFiles = ( await fs.readdir(pluginsPath) ).filter(file => /\.ts$/.test(file))
+        const plugins: Array<BasePlugin | MonitoringPlugin> = []
 
-    for (const file of pluginFiles) {
-        const pluginPath = path.join(pluginsPath, file)
-        const pluginModulePath = path.relative(__dirname, pluginPath).replace(/\.ts$/, "")
+        for (const file of pluginFiles) {
+            const pluginPath = path.join(pluginsPath, file)
+            const pluginModulePath = path.relative(__dirname, pluginPath).replace(/\.ts$/, "")
 
-        console.log(`Loading plugin from "${pluginPath}"...`)
+            console.log(`Loading plugin from "${pluginPath}"...`)
 
-        try {
-            const module = await import(pluginModulePath)
-            const PluginClass = module.default
-            const pluginInstance = new PluginClass()
-            plugins.push(pluginInstance)
-        } catch (error) {
-            console.error(`Failed to load plugin from "${pluginPath}":`, error)
+            try {
+                const module = await import(pluginModulePath)
+                const PluginClass = module.default
+                const pluginInstance = new PluginClass()
+                plugins.push(pluginInstance)
+            } catch (error) {
+                console.error(`Failed to load plugin from "${pluginPath}":`, error)
+            }
         }
+        return plugins
     }
-
-    return plugins
 }
-
-export default loadPlugins
