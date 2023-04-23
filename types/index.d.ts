cd7ca26de01f6611c83e10d92e9922c70561222b
Larry W <l422y@l422y.com>
2023-04-22 20:47:49 -0400

    Add types for the @daemonitor/plugins module, which includes abstract classes for PluginConfigProvider, MonitoringPlugin, and BasePlugin, as well as a PluginManager class with methods for initializing, loading, setting up, monitoring, refreshing, and tearing down plugins

---
+++ types/index.d.ts
@@ -0,0 +1,68 @@
+declare module "@daemonitor/plugins" {
+    import { IConnector } from "@daemonitor/common"
+
+    export abstract class PluginConfigProvider {
+        static get(alias: string): any
+    }
+
+    export abstract class MonitoringPlugin extends BasePlugin {
+        static currentPluginIndex: number
+        protected instanceName: string
+        protected refreshTimer: any
+        protected uniqueId: string
+        protected pluginIndex: number
+
+        protected constructor(alias: string, name: string, description: string)
+
+        public send(data: any, unique_id?: string): Promise<void>
+
+        abstract setup(): Promise<void>;
+
+        abstract monitor(): Promise<any>;
+
+        abstract refresh(): Promise<void>;
+
+        abstract teardown(): Promise<void>;
+
+        getName(): string
+    }
+
+
+    export class PluginManager {
+        static API_CONNECTIONS: IConnector[]
+
+        initialize(): Promise<void>
+
+        loadPlugins(): Promise<void>
+
+        setupAll(): Promise<void>
+
+        monitorAll(): Promise<any>
+
+        addApiConnection(apiConnection: IConnector): Promise<void>
+
+        refreshAll(): Promise<void>
+
+        teardownAll(): Promise<void>
+    }
+
+    export abstract class BasePlugin {
+        static currentPluginIndex: number
+        protected alias: string
+        protected name: string
+        protected description: string
+        protected config: any
+
+        protected constructor(alias: string, name: string, description: string)
+
+        abstract setup(): Promise<void>;
+
+        abstract monitor(): Promise<any>;
+
+        abstract refresh(): Promise<void>;
+
+        abstract teardown(): Promise<void>;
+
+        getName(): string
+    }
+}
