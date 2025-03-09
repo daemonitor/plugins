619ae1fee25d4d372a9e99009cadf13923158635
Larry Williamson <l422y@l422y.com>
2025-03-09 14:16:30 -0400

    feat: add nuxt and vue devDependencies, update plugins and .gitignore

--- plugins/index.ts
+++ plugins/index.ts
@@ -3,8 +3,12 @@ import { OSPlugin } from "./monitoring/OSPlugin.js"
 import { PM2Plugin } from "./monitoring/PM2Plugin.js"
 import { MongoDBPlugin } from "./monitoring/MongoDBPlugin.js"
 import { WebsitePlugin } from "./monitoring/WebsitePlugin.js"
+import { DockerPlugin } from "./monitoring/DockerPlugin.js"
+import { CloudflarePlugin } from "./monitoring/CloudflarePlugin.js"
 
 export default {
+    CloudflarePlugin,
+    DockerPlugin,
     EwelinkPlugin,
     MongoDBPlugin,
     OSPlugin,
